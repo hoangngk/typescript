41387cb8bf06be4ca2a92f269ad3845f806d1ff5
Hoang Nguyen Khanh <hnguyenkhanh@openwt.com>
2017-02-16 09:47:06 +0700

    initial

---
+++ typscript-gulp/src/main.ts
@@ -0,0 +1,9 @@
+import { sayHello } from './greet';
+
+function hello(compiler: string) {
+    console.log(`Hello from ${compiler}`);
+}
+
+hello("TypeScript")
+
+console.log(sayHello("TypeScript"));
